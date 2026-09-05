c5f5d51130fbfe2e44131d752c6fcb6bd05342c6
siyab Ahmad <siyabkhan10492@gmail.com>
2026-09-05 19:42:20 +0500

    javascript lec

---
+++ lec 20/script.js
@@ -0,0 +1,13 @@
+// what is javascript
+
+// javascript is programming language wihch make web interactive
+
+let userName= "ahmad"
+
+console.log(userName)
+
+
+let s = 2
+let d = 5
+
+console.log(s + d)
