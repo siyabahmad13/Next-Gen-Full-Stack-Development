cccd1cc7f467d2a460962da8a606d348595d5d4b
siyab Ahmad <siyabkhan10492@gmail.com>
2026-09-25 19:01:09 +0500

    lec 25

---
+++ lec 25/script.js
@@ -0,0 +1,48 @@
+
+// for(let i = 1 ; i<= 10; i += 2 ){
+//   console.log(i)
+// }
+
+// let i = 6;
+
+// while(i <= 5){
+//     console.log(i)
+//     i++;
+// }
+
+
+// for(let i = 1 ; i <5 ; i++){
+//     console.log(i)
+// }
+
+
+// let pasword = ""
+
+// while(password !== "123"){
+//     password = prompt("please enter your password")
+// }
+
+
+// let i = 6
+
+// do {
+//     console.log(i)
+//     i++;
+// }
+// while(i <=5 )
+
+
+// for(let i = 1; i <= 10; i++){
+//     if(i === 3){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+for(let i = 1; i <=3 ; i++) {
+
+    for (let j = 1; j <=10; j++){
+
+        console.log(i + "x" + j + " = " + (i * j))
+    }
+}
